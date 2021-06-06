4d5f82ef7f09ae5f5194de2688f8ff1db777ba73
Meco632 <Mia@Ms-MacBook-Air.local>
2021-06-06 21:49:55 +0100

    Form function added to JS so that city can be called from API. UI then updated.

--- src/app.js
+++ src/app.js
@@ -45,8 +45,19 @@ function displayTemperature(response) {
   //precipitationElement = response.data.precipitation.mode;
 }
 
-let apiKey = "e17801e95b80791ab63aefa29a2220f3";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "e17801e95b80791ab63aefa29a2220f3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);

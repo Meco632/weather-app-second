94873005a826e5a14e90cf5fb2ff595c91e19f1c
Meco632 <Mia@Ms-MacBook-Air.lan>
2021-06-07 19:40:51 +0100

    Added array of days and Javascript loop. Loop is from html file, moved row outside and then moved close div outside and deleted surplus. New function is called displayForecast.

--- src/app.js
+++ src/app.js
@@ -22,6 +22,36 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `   
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecaast-temperature-max"> 20° </span>
+
+                  <span class="weather-forecaast-temperature-min"> 12° </span>
+                </div>
+              </div>
+            `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -91,3 +121,4 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 let celsiusTemperature = null;
 
 search("New York");
+displayForecast();

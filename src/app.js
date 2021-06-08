63330e2d8e131ae2d10a6a993bed4ffc8739159d
Meco632 <Mia@Ms-MacBook-Air.lan>
2021-06-08 19:48:38 +0100

    Stored response from API inside varaiable called forecast. Looped through this variable rather than the days as before. Loop through each and each is an object with data. Getting the index for the forEach function which also has index for item in the array. Formatting the day of day and datastamp. Also including icon name inside image URL and rounding max and min temperatures. Also ensure only 6 show and if greater then not displaying that day.

--- src/app.js
+++ src/app.js
@@ -22,30 +22,50 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+
+  let days = ["sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `   
+  //let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `   
               <div class="col-2">
-                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-date">${formatDay(
+                  forecastDay.dt
+                )}</div>
+              
                 <img
-                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
                   alt=""
                   width="42"
                 />
                 <div class="weather-forecast-temperatures">
-                  <span class="weather-forecaast-temperature-max"> 20° </span>
+                  <span class="weather-forecaast-temperature-max"> ${Math.round(
+                    forecastDay.temp.max
+                  )}° </span>
 
-                  <span class="weather-forecaast-temperature-min"> 12° </span>
+                  <span class="weather-forecaast-temperature-min"> ${Math.round(
+                    forecastDay.temp.min
+                  )}° </span>
                 </div>
               </div>
             `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
